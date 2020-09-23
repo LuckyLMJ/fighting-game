0f08b347633e5da65b42b3bc2c1d6e85b01d13ae
LuckyLMJ <70863143+LuckyLMJ@users.noreply.github.com>
2020-09-23 13:00:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -93,8 +93,8 @@ function draw() {
   text(eName, windowWidth / 2 + 10, windowHeight - 55)
   enemyPicture = loadImage(ePic);
   playerPicture = loadImage("player.png");
-  image(enemyPicture, windowWidth / 2, windowHeight / 2);
-  image(playerPicture, 100, windowHeight / 2)
+  image(enemyPicture, windowWidth / 2, windowHeight / 2, 256, 256);
+  image(playerPicture, 100, windowHeight / 2, 256, 256)
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
